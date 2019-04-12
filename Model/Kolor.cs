acef8edafc4d13f6ca4e1b169bef68c8a1772838
Roqche <neez1998@gmail.com>
2019-04-12 17:22:37 +0200

    12.04.2019

---
+++ Model/Kolor.cs
@@ -0,0 +1,16 @@
+﻿namespace KoloryWPF.Model
+{
+    public class Kolor
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+
+        public Kolor (byte r, byte g, byte b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+    }
+}
---
+++ ModelView/EdycjaKoloru.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Media;
+
+namespace KoloryWPF.ModelView
+{
+    using Model;
+
+    public class EdycjaKoloru
+    {
+        private readonly Kolor kolor = Ustawienia.Czytaj();
+
+        public byte R
+        {
+            get => kolor.R;
+            set => kolor.R = value;
+        }
+
+        public byte G
+        {
+            get => kolor.G;
+            set => kolor.G = value;
+        }
+
+        public byte B
+        {
+            get => kolor.B;
+            set => kolor.B = value;
+        }
+
+        public Color Color => kolor.ToColor();
+
+        public void Zapisz()
+        {
+            Ustawienia.Zapisz(kolor);
+        }
+    }
+
+    public static class Rozszerzenia
+    {
+        public static Color ToColor(this Kolor kolor)
+        {
+            return new Color()
+            {
+                A = 255,
+                R = kolor.R,
+                G = kolor.G,
+                B = kolor.B
+            };
+        }
+    }
+}
